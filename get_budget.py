83f1ee5e022e972d72b72083128fcd8ca8853439
Yothin M <m@yothinix.com>
2018-09-29 17:43:03 +0700

    Change dict.get to [] :rocket:

--- get_budget.py
+++ get_budget.py
@@ -17,16 +17,15 @@ def get_budget(first_day, last_day):
         budget_of_full_month = 0
         month_number = first_day.month
         while range_of_month > 0:
-            budget_of_full_month += budgets.get(month_number)
+            budget_of_full_month += budgets[month_number]
             month_number += 1
             range_of_month -= 1
 
-        budget_month_of_last = budgets.get(last_day.month)
-        day_of_month_of_last = day_of_month.get(last_day.month)
+        budget_month_of_last = budgets[last_day.month]
+        day_of_month_of_last = day_of_month[last_day.month]
 
         budget_per_day_of_last = (budget_month_of_last/round(day_of_month_of_last, 2))
         return round(budget_of_full_month + budget_per_day_of_last*last_day.day, 2)
-
-    elif range_of_month == 0:
-        budget = (budgets.get(first_day.month)/round(30, 2))*range_of_day
+    else:
+        budget = (budgets[first_day.month]/round(day_of_month[first_day.month], 2))*range_of_day
         return round(budget, 2)
